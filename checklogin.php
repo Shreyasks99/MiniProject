25f90a844455433a6431dcb9f8c0ec23b2e53db8
unknown <shreyasks99@gmail.com>
2019-11-03 21:59:21 +0530

    First Commit

---
+++ checklogin.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$host="localhost";
+$email="";
+$password="";
+$db_name="test";
+$tbl_name="mails";
+$con=mysqli_connect("$host","$email","$password")or die("cannot connect");
+$select_db=mysqli_select_db($con,$db_name) or die("cannot select DB");
+
+if(isset($_POST['email']) && isset($_POST['password']))
+{
+	$email=$_POST['email'];
+	$pswd=$_POST['password'];
+	$ab="SELECT * FROM mails WHERE email='$email' and password='$pswd'";
+	$result=mysqli_query($con,$ab) or die(mysqli_error($con));
+	$cnt=mysqli_num_rows($result);
+	if($cnt == true)
+	{
+		header('Location:upload.html');
+	}
+	else
+	{
+		echo '<script language="javascript">';
+		echo 'alert("Wrong Credentials Entered");';
+		echo 'window.location.href="mainlogin.html";';
+		echo '</script>';
+	}
+}
+ob_end_flush();
+?>
+	
